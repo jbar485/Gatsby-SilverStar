dd9f49d9f81934e071af37418527aace2bdc7d8d
Judah Barton <judah.barton@yahoo.com>
2020-04-07 16:28:57 -0700

    Add some global css to fix the errors

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { useStaticQuery, graphql } from "gatsby";
 
 import "./layout.css";
 import Header from './Globals/Header';
@@ -13,9 +11,5 @@ const Layout = ({ children }) => (
 <Footer />
 </>
 );
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
 
 export default Layout
