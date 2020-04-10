8b6d23fcd5430827cb3bd2a36789d8fd6bb0b950
Judah Barton <judah.barton@yahoo.com>
2020-04-10 11:23:11 -0700

    add fonts

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 import "./layout.css";
+import { Helmet } from "react-helmet";
 import Header from './Globals/Header';
 import Footer from './Globals/Footer';
 
 const Layout = ({ children }) => (
 <>
+<Helmet>
+<link href="https://fonts.googleapis.com/css2?family=Yanone+Kaffeesatz&display=swap" rel="stylesheet"></link>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,300&family=Yanone+Kaffeesatz&display=swap" rel="stylesheet"></link>
+</Helmet>
 <Header />
 {children}
 <Footer />
